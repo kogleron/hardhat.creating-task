d3140a88e6e7708fb69f95e91a85ff739b1a454d
kogleron <kogleron@gmail.com>
2022-06-06 19:20:54 +0300

    Define a parameter's type

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
-const { task } = require("hardhat/config");
+const { task, types } = require("hardhat/config");
 require("@nomiclabs/hardhat-web3");
 
 task("balance", "Prints an account's balance")
   .addParam("account", "The account's address")
   .addOptionalParam("greeting", "The greeting to print before balance", "Hi, there")
+  .addOptionalParam("times", "The number of times to print the greeting", 1, types.int)
   .setAction(async (taskArgs) => {
-    console.log(taskArgs.greeting);
+
+    for (let i = 0; i < taskArgs.times; i++) {
+      console.log(taskArgs.greeting);
+    }
 
     const account = web3.utils.toChecksumAddress(taskArgs.account);
     const balance = await web3.eth.getBalance(account);
