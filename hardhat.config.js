d71f5b01ab882cdcf0608e108bff4b7b15de28b5
kogleron <kogleron@gmail.com>
2022-06-06 19:14:13 +0300

    Add optional parameter to the task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
+const { task } = require("hardhat/config");
 require("@nomiclabs/hardhat-web3");
 
 task("balance", "Prints an account's balance")
   .addParam("account", "The account's address")
+  .addOptionalParam("greeting", "The greeting to print before balance", "Hi, there")
   .setAction(async (taskArgs) => {
+    console.log(taskArgs.greeting);
+
     const account = web3.utils.toChecksumAddress(taskArgs.account);
     const balance = await web3.eth.getBalance(account);
 
